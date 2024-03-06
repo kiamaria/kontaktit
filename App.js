250ddb96f76cdb99a99b9501f70ad4b756197d41
kiamaria <kia@hietamaki.org>
2024-03-06 14:17:39 +0200

    Valmis

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import { useState } from 'react';
+
 
 export default function App() {
+
+  const [contact, setContact] = useState();
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync(
+        {
+          fields: [Contacts.Fields.PhoneNumbers]
+        }
+      );
+      if (data.length > 0) {
+        setContact(data);
+      }
+    }
+  }
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        style={styles.list}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.name}>{item.name}</Text>
+            {item.phoneNumbers && item.phoneNumbers.length > 0 && (
+              <Text>{item.phoneNumbers[0].number}</Text>
+            )}
+          </View>
+        )}
+        data={contact}
+      />
+      <Button title="Get contact" onPress={getContacts} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +47,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    paddingTop: 50
+  },
+  name: {
+    fontWeight: 'bold'
   },
 });
